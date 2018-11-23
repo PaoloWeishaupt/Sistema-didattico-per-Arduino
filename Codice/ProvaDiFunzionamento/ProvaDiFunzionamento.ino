ffcf81b59d0aa21ecf5e9c57373e3193f305a5fc
CarloPezzotti <carlo.pezzotti@samtrevano.ch>
2018-11-23 14:01:16 +0100

    Update Analisi
    Added chapter analisi dei requisiti.
    
    Co-Authored-By: paoloweishaupt <paoloweishaupt@users.noreply.github.com>

--- Codice/ProvaDiFunzionamento/ProvaDiFunzionamento.ino
+++ Codice/ProvaDiFunzionamento/ProvaDiFunzionamento.ino
@@ -1,22 +1,10 @@
-#include "DigiKeyboard.h"
-#define MOD_SHIFT_LEFT      (1<<1)  //00000010
-#define MOD_GUI_LEFT        (1<<3)  //00001000
-#define MOD_CONTROL_LEFT    1
-#define KEY_ENTER   40
-#define KEY_C       6
-#define KEY_M       16
-#define KEY_D       7
 void setup() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cmd");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  pinMode(5,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-   
+   if(digitalRead(5) == HIGH){
+    DigiKeyboard.println("Hello Digispark!");
+   }
 }
