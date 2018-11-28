961c062d4be7dd4bda4a516d689deecbfcbb8667
CarloPezzotti <carlo.pezzotti@samtrevano.ch>
2018-11-28 14:38:47 +0100

    Upload getButtonState and writeRGBLed

--- Codice/GetButtonState/GetButtonState.ino
+++ Codice/GetButtonState/GetButtonState.ino
@@ -1,15 +1,37 @@
+int r = 0;
+int g = 0;
+int b = 0;
 void setup()
 {
-  pinMode(3,OUTPUT);
+  Serial.begin(9600);
 }
 
 
 void loop()
 {
-  digitalWrite(3,getButtonState(5));
+  if(getButtonState(52)){
+    r++;
+  }
+  if(getButtonState(50)){
+    g++;
+  }
+  if(getButtonState(48)){
+    b++;
+  }
+  accendiLed(r,g,b,2,3,4);
+  delay(20);
 }
 
 boolean getButtonState(int pin){
   pinMode(pin,INPUT);
   return digitalRead(pin);
+}
+
+void accendiLed(int r, int g, int b,int pinRed,int pinGreen,int pinBlue){
+  pinMode(pinRed,OUTPUT);  
+  pinMode(pinBlue,OUTPUT);  
+  pinMode(pinGreen,OUTPUT);  
+  analogWrite(pinGreen,255-g);
+  analogWrite(pinRed,255-r);
+  analogWrite(pinBlue,255-b);
 }
